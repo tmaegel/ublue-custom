1ec9ab1778909a7a1724f8da1b7fcdc6719d8ff6
Toni Mägel <tmaegel@posteo.de>
2026-04-11 12:09:31 +0200

    fix(tailscale): Script

--- build_files/tailscale.sh
+++ build_files/tailscale.sh
@@ -5,7 +5,7 @@ set -ouex pipefail
 echo "::group:: ===$(basename "$0")==="
 
 dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
-dnf install tailscale
+dnf -y install tailscale
 
 systemctl enable tailscaled
 
